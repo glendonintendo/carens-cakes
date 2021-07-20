2826f59b15c337e41666c15e4f995922917e4972
Glen Eggers <gleneggers64@gmail.com>
2021-07-20 14:07:15 -0500

    add routes and Nav

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,30 @@
-function App() {
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import Nav from "./components/Nav";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import NoMatch from "./pages/NoMatch";
+import Explore from "./pages/Explore";
+import Order from "./pages/Order";
+import Contact from "./pages/Contact";
+
+const App = () => {
   return (
-    <div>
-      Project 3 bb
-    </div>
+    <Router>
+      <div>
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/home" component={Home} />
+          <Route exact path="/explore" component={Explore} />
+          <Route exact path="/order" component={Order} />
+          <Route exact path="/contact" component={Contact} />
+          <Route component={NoMatch} />
+        </Switch>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
