cf0ebb67a4eb47328e43be8c7dde3f8f624e7579
Glen Eggers <gleneggers64@gmail.com>
2021-07-20 15:40:24 -0500

    add drawer component overlay for Cart

---
+++ client/src/components/Cart/index.js
@@ -0,0 +1,51 @@
+import { useRef } from "react";
+import { IoCart } from "react-icons/io5";
+import {
+  IconButton,
+  Button,
+  Input,
+  Drawer,
+  DrawerBody,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  useDisclosure,
+} from "@chakra-ui/react";
+
+const Cart = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const btnRef = useRef();
+
+  return (
+    <>
+      <IconButton as={IoCart} boxSize={50} onClick={onOpen} bg="transparent" />
+      <Drawer
+        isOpen={isOpen}
+        placement="right"
+        onClose={onClose}
+        finalFocusRef={btnRef}
+      >
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton />
+          <DrawerHeader>Create your account</DrawerHeader>
+
+          <DrawerBody>
+            <Input placeholder="Type here..." />
+          </DrawerBody>
+
+          <DrawerFooter>
+            <Button variant="outline" mr={3} onClick={onClose}>
+              Cancel
+            </Button>
+            <Button colorScheme="blue">Save</Button>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+    </>
+  );
+};
+
+export default Cart;
